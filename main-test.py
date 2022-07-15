4b0e43b12b4e9738cc938930fc0332f5d4969c57
Devoteng1 <lovebergers@gmail.com>
2022-07-15 22:09:58 +0000

    commit 3

--- main-test.py
+++ main-test.py
@@ -2,7 +2,7 @@ from main import Add
 
 def TestAdd():
     assert Add(2,3) == 5
-    assert Add(2,3) == 11
+    assert Add(2,3) == 5
     print("Add function works correctly")
 
 if __name__ == '__main__':
